87c46c609be8875c5c7aea768a9814ea4a93088a
torakyun <leo@MacBook-Pro-5.stama1.kt.home.ne.jp>
2022-10-15 19:07:22 +0900

    discromonatorのパッケージ追加

--- demucs/models/__init__.py
+++ demucs/models/__init__.py
@@ -1,2 +1,7 @@
 from .demucs import *  # NOQA
 from .tasnet import *  # NOQA
+
+from .parallel_wavegan_discriminator import * # NOQA
+from .hifigan_discriminator import * # NOQA
+from .melgan_discriminator import * # NOQA
+from .style_melgan_discriminator import * # NOQA
