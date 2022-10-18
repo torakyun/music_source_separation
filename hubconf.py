b0a66aec935acfed59fc6632589bdeca4e11971f
torakyun <toraki1998m@yahoo.co.jp>
2022-10-18 13:53:35 +0900

    不要なファイルを削除

--- hubconf.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-dependencies = ['diffq', 'julius', 'torch', 'tqdm']
-
-from demucs.pretrained import demucs, tasnet  # noqa
