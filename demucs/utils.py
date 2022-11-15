28e1acedb310a012a125d4920106d0f147e3d83b
torakyun <toraki1998m@yahoo.co.jp>
2022-11-15 14:14:37 +0900

    unfoldの追加

--- demucs/utils.py
+++ demucs/utils.py
@@ -83,6 +83,22 @@ def center_trim(tensor: torch.Tensor, reference: tp.Union[torch.Tensor, int]):
     return tensor
 
 
+def unfold(a, kernel_size, stride):
+    """Given input of size [*OT, T], output Tensor of size [*OT, F, K]
+    with K the kernel size, by extracting frames with the given stride.
+    This will pad the input so that `F = ceil(T / K)`.
+    see https://github.com/pytorch/pytorch/issues/60466
+    """
+    *shape, length = a.shape
+    n_frames = math.ceil(length / stride)
+    tgt_length = (n_frames - 1) * stride + kernel_size
+    a = F.pad(a, (0, tgt_length - length))
+    strides = list(a.stride())
+    assert strides[-1] == 1, 'data should be contiguous'
+    strides = strides[:-1] + [stride, 1]
+    return a.as_strided([*shape, n_frames, kernel_size], strides)
+
+
 def average_metric(metric, count=1.):
     """
     Average `metric` which should be a float across all hosts. `count` should be
